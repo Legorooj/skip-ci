67b9db79ceae9581c0929138ade4c71b1680d6b4
Legorooj <legorooj@protonmail.com>
2020-11-06 18:55:33 +1100

    Add GitHub action code and config

---
+++ index.js
@@ -0,0 +1,50 @@
+// Copyright (c) 2020 Legorooj. 
+// This file is licensed under the terms of the Apache License, version 2.0
+const core = require("@actions/core");
+const github = require("@actions/github");
+
+async function main() {
+    // Create the RegExp object, and load the octokit
+    const pattern = core.getInput('pattern');
+    const token = core.getInput('token')
+
+    // Make sure the inputs have loaded properly
+    if (!pattern) {
+        core.setFailed("Unable to load pattern.");
+        return 1;
+    } else if (!token) {
+        core.setFailed("Unable to load token")
+        return 1;
+    }
+    
+    const re = RegExp(pattern);
+    const octokit = github.getOctokit(token);
+    
+    // Fetch the workflow run object
+    const { data: current_run } = octokit.actions.getWorkflowRun(
+        {
+            owner: github.context.repo.owner, 
+            repo: github.context.repo.repo,
+            run_id: github.context.runId
+        }
+    );
+
+    // Extract the commit message from the commit which triggered the workflow run
+    const commit_message = current_run.head_commit.message;
+
+    // And check for the pattern.
+    let match = commit_message.search(re);
+    if (match) {
+        core.setOutput("canSkip", "true");
+        console.log(`Skipping; match found: ${match}`)
+    } else {
+        core.setOutput("canSkip", "false")
+        console.log("Not skipping, no match found.")
+    }
+}
+
+try{
+    main();
+} catch (e) {
+    core.setFailed(e.message)
+}
